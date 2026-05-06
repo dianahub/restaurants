9dc90e833c23bed21ab71e7294aa64042dc15c3b
Diana Castillo <dianahelene@gmail.com>
2026-05-06 17:10:51 -0400

    fix: shim Node.js core modules (fs, path, os, etc.) for Metro bundler

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
+const emptyShim = path.resolve(__dirname, "shims/empty.js");
+
 config.resolver.extraNodeModules = {
   assert: require.resolve("assert"),
+  fs: emptyShim,
+  path: emptyShim,
+  os: emptyShim,
+  net: emptyShim,
+  tls: emptyShim,
+  child_process: emptyShim,
+  dns: emptyShim,
+  http2: emptyShim,
 };
 
 module.exports = config;
