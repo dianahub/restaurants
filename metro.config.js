621229879f454b2ef9c8143a603eff4cf16939cd
Diana Castillo <dianahelene@gmail.com>
2026-05-06 17:21:05 -0400

    fix: shim all Node.js modules required by @irys/sdk

--- metro.config.js
+++ metro.config.js
@@ -7,6 +7,7 @@ const emptyShim = path.resolve(__dirname, "shims/empty.js");
 
 config.resolver.extraNodeModules = {
   assert: require.resolve("assert"),
+  // Node-only modules used by @irys/sdk and other Metaplex transitive deps
   fs: emptyShim,
   path: emptyShim,
   os: emptyShim,
@@ -15,6 +16,18 @@ config.resolver.extraNodeModules = {
   child_process: emptyShim,
   dns: emptyShim,
   http2: emptyShim,
+  stream: emptyShim,
+  zlib: emptyShim,
+  http: emptyShim,
+  https: emptyShim,
+  crypto: emptyShim,
+  readline: emptyShim,
+  worker_threads: emptyShim,
+  cluster: emptyShim,
+  dgram: emptyShim,
+  "csv-parse": emptyShim,
+  "csv-stringify": emptyShim,
+  inquirer: emptyShim,
 };
 
 module.exports = config;
